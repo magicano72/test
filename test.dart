5aeec5a5b4b5ab602dce85f237438ea9df7b9c64
magicano72 <mostafahany108889@gmail.com>
2025-02-10 11:23:18 +0200

    commit 2

--- test.dart
+++ test.dart
@@ -1,6 +1,43 @@
-class Test {
+abstract class common {
+  void printName();
+}
+
+class person extends common {
   final int? id;
   final String? name;
   final String? country;
-  Test(this.country, {this.id, this.name});
+  person({
+    this.id,
+    this.name,
+    this.country,
+  });
+  @override
+  void printName() {
+    print("Person Name: $name");
+  }
+}
+
+class animal extends common {
+  final int? high;
+  final String? name;
+  final String? country;
+  animal(this.country, {this.high, this.name});
+  @override
+  void printName() {
+    print("Animal Name: $name");
+  }
+}
+
+class printer {
+  void printName(common c) {
+    c.printName();
+  }
+}
+
+void main() {
+  person p = person(id: 1, name: "Ali", country: "Pakistan");
+  animal a = animal("Pakistan", high: 5, name: "Lion");
+  printer pr = printer();
+  pr.printName(p);
+  pr.printName(a);
 }
